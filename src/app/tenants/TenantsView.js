77c05b2b00595913fac1519f3d8135c4f7a1a2d1
Pravin <pravinranjansingh@gmail.com>
2019-11-16 23:56:43 +1300

    added tenants table

--- src/app/tenants/TenantsView.js
+++ src/app/tenants/TenantsView.js
@@ -1,7 +1,76 @@
 import React from 'react';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Checkbox from '@material-ui/core/Checkbox';
 
 import TenantView from './TenantView';
 
-const TenantsView = ({ tenants }) => tenants.map(tenant => <TenantView key={tenant.id} tenant={tenant} />);
+const useStyles = makeStyles(theme => ({
+  root: {
+    width: '100%',
+    marginTop: theme.spacing(3),
+  },
+  paper: {
+    width: '100%',
+    marginBottom: theme.spacing(2),
+  },
+  table: {
+    minWidth: 750,
+  },
+  tableWrapper: {
+    overflowX: 'auto',
+  },
+  visuallyHidden: {
+    border: 0,
+    clip: 'rect(0 0 0 0)',
+    height: 1,
+    margin: -1,
+    overflow: 'hidden',
+    padding: 0,
+    position: 'absolute',
+    top: 20,
+    width: 1,
+  },
+}));
 
-export default TenantsView;
+export default function TenantsView({ tenants }) {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <Paper className={classes.paper}>
+        <div className={classes.tableWrapper}>
+          <Table className={classes.table} aria-labelledby="tableTitle" size="medium" aria-label="enhanced table">
+            <TableHead>
+              <TableRow>
+                <TableCell padding="checkbox">
+                  <Checkbox checked={false} />
+                </TableCell>
+                <TableCell>Tenants Name</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {tenants.map(({ name, id }) => {
+                return (
+                  <TableRow key={id}>
+                    <TableCell padding="checkbox">
+                      <Checkbox checked={false} />
+                    </TableCell>
+                    <TableCell component="th" scope="row" padding="none">
+                      {name}
+                    </TableCell>
+                  </TableRow>
+                );
+              })}
+            </TableBody>
+          </Table>
+        </div>
+      </Paper>
+    </div>
+  );
+}
