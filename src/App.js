ff560511f2adac4508d5c4be3df3ddfa30dc1026
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2019-11-16 20:06:20 +1300

    Added redux related libraries
    redux
    redux-form
    redux-saga
    redux-extension

--- src/App.js
+++ src/App.js
@@ -2,19 +2,23 @@ import React from 'react';
 import { ThemeProvider } from '@material-ui/styles';
 import { createBrowserHistory } from 'history';
 import { Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 import theme from './theme';
+import { reduxStore } from './framework/redux';
 import Routes from './Routes';
 import './App.css';
 
 const browserHistory = createBrowserHistory();
 
 const App = () => (
-  <ThemeProvider theme={theme}>
-    <Router history={browserHistory}>
-      <Routes />
-    </Router>
-  </ThemeProvider>
+  <Provider store={reduxStore}>
+    <ThemeProvider theme={theme}>
+      <Router history={browserHistory}>
+        <Routes />
+      </Router>
+    </ThemeProvider>
+  </Provider>
 );
 
 export default App;
